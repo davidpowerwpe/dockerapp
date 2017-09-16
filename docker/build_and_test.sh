f28e30ef60e8f161f89f9d550c44eef25ce5c872
David Power <david.power@wpengine.com>
2017-09-17 00:41:02 +0100

    Debug path

--- docker/build_and_test.sh
+++ docker/build_and_test.sh
@@ -5,7 +5,7 @@ set -eu
 docker build -t ${PROJECT_NAME} .
 
 # Run tests, also bringing up container dependencies
-docker run ${PROJECT_NAME} /dockerapp/docker/setup_and_run.sh
+docker run ${PROJECT_NAME} /docker/setup_and_run.sh
 
 # Stop and remove the containers
 docker stop ${PROJECT_NAME} && docker rmi ${PROJECT_NAME}
