525f7fa9c764f7d869e80c9a9335633595c42780
David Power <david.power@wpengine.com>
2017-09-17 01:06:43 +0100

    Update build and test

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     environment {
-        PROJECT_NAME="dockerapp-$BUILD_TAG"
+        PROJECT_NAME="dockerapp_$BUILD_TAG"
     }
 
     stages {
--- docker/build_and_test.sh
+++ docker/build_and_test.sh
@@ -2,10 +2,10 @@
 set -eu
 
 # Build the project containers
-docker build -t ${PROJECT_NAME} .
+docker build -t ${PROJECT_NAME}:latest .
 
 # Run tests, also bringing up container dependencies
-docker run ${PROJECT_NAME} /docker/setup_and_run.sh
+docker run ${PROJECT_NAME} /docker/run_tests.sh
 
 # Stop and remove the containers
 docker stop ${PROJECT_NAME} && docker rmi ${PROJECT_NAME}
