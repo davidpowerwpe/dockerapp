ad6c0cb604463ad829caed4b9edba8d7868fa8b6
David Power <david.power@wpengine.com>
2017-09-18 12:24:15 +0100

    improve readability of texts

--- spec/helloworld_spec.rb
+++ spec/helloworld_spec.rb
@@ -2,19 +2,19 @@ require 'helloworld'
 RSpec.describe HelloWorld do
   let('hello') { HelloWorld.new }
 
-  describe 'an instanciated hello world' do
-      context 'hello method' do
+  describe 'an instanciated hello world.' do
+      context 'the hello method' do
         it 'returns a hello world string' do
           expect(hello.hello).to eq('hello world')
         end
       end
 
-      context 'hellowWithName method' do
+      context 'the hellowWithName method' do
         it 'returns a hello world string with given name' do
           expect(hello.helloWithName('James')).to eq('hello world, James')
         end
 
-        it 'returns a hello world string with given no name when given empty string' do
+        it 'returns a hello world string with given no name when given an empty string' do
           expect(hello.helloWithName('')).to eq('hello world, ')
         end
       end
